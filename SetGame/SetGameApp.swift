eeb39d9d6938f1ac0099ab3636078fe2b8801664
Joao Marco <joaomarcobarros@gmail.com>
2023-05-03 23:16:11 +0200

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Joao Barros on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
